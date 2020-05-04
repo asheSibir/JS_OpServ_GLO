beaf57efce7d6d37936df307ffc76cc68078a4c4
asheSibir <56887042+asheSibir@users.noreply.github.com>
2020-05-04 16:10:14 +0300

    draft

--- ДОПОЛНИТЕЛЬНОЕ ДЗ/script.js
+++ ДОПОЛНИТЕЛЬНОЕ ДЗ/script.js
@@ -5,24 +5,39 @@ document.addEventListener('DOMContentLoaded', () => {
         output = document.getElementById('output');
 
     select.addEventListener('change', () => {
-        const request = new XMLHttpRequest();
-        request.open('GET', './cars.json');
-        request.setRequestHeader('Content-type', 'application/json');
-        request.send();
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                data.cars.forEach(item => {
-                    if (item.brand === select.value) {
-                        const {brand, model, price} = item;
-                        output.innerHTML = `Тачка ${brand} ${model} <br>
-                        Цена: ${price}$`;
+        const chooseCar = (data) => {
+            return new Promise((resolve, reject) => {
+                const request = new XMLHttpRequest();
+                request.open('GET', './cars.json');
+                request.setRequestHeader('Content-type', 'application/json');
+                request.send();
+                request.addEventListener('readystatechange', () => {
+                    if (request.readyState === 4 && request.status === 200) {
+                        const data = JSON.parse(request.responseText);
+                        // console.log(request.readyState);
+                        // console.log(request.status);
+                        console.log(data);
+                        return resolve(data);
+                        // data.cars.forEach(item => {
+                        //     if (item.brand === select.value) {
+                        //         const {brand, model, price} = item;
+                        //         output.innerHTML = `Тачка ${brand} ${model} <br>
+                        //         Цена: ${price}$`;
+                        //     }
+                        // });
+                    } else {
+                        reject(request.responseText);
                     }
                 });
-            } else {
-                output.innerHTML = 'Произошла ошибка';
-            }
-        });
+            });
+        };
+        const data = select.value;
+        const getResonse = (key) => {
+            
+            console.log(key);
+        };
+        chooseCar(data)
+            .then(getResonse)
+            .catch(error => output.innerHTML = 'error');
     });
-
 });
